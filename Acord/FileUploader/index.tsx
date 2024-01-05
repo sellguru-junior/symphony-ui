42a7c75a598fb91ae97b8cb3b5c63d7a7bfaf206
amir <amirdehghani137722@gmail.com>
2024-01-05 17:14:48 +0330

    add fileUploader

--- Acord/FileUploader/index.tsx
+++ Acord/FileUploader/index.tsx
@@ -38,33 +38,44 @@ const FileUploadr: React.FC<FileUploadrProps> = ({ children, theme, ...props }) 
                   </div>                                         
                   :
                   <div className={`${theme}-FileUploader-uploadBox-box ${files.length >0 ? `${theme}-FileUploader-uploadBox-fileExist`:undefined}`}>
-                      <div className={`flex justify-center items-center w-full h-full `}>
-                          <div className="grid">
+                      <div style={{
+                        display:'flex',
+                        justifyContent:'center',
+                        alignItems:'center',
+                        width:'100%',
+                        height:'100%'
+                      }}>
+                          <div style={{display:'grid'}}>
                             <div className={`${theme}-FileUploader-icon`}></div>
-                              <div className="text-[14px] text-center text-[#0F0F0F] font-bold">
-                                  Drag & drop files or <span className="text-[#00B5FB] cursor-pointer">Browse</span> 
+                              <div style={{
+                                fontSize:'14px',
+                                textAlign:'center',
+                                color:'#0F0F0F',
+                                fontWeight:'500'
+                              }}>
+                                  Drag & drop files or <span style={{color:'#00B5FB',cursor:'pointer'}}>Browse</span> 
                               </div>
-                              <div className="flex justify-center items-center mt-2">
-                                  <div className="text-[#7B93AF] text-center text-[12px]">Supported formates: JPEG, PNG, GIF, MP4, PDF, PSD, AI, Word, PPT</div>
+                              <div className={`${theme}-FileUploader-uploader-suportText-container`}>
+                                  <div className={`${theme}-FileUploader-uploader-suportText`}>Supported formates: JPEG, PNG, GIF, MP4, PDF, PSD, AI, Word, PPT</div>
                               </div>
                           </div>
                       </div>
                       <input  onChange={(res:any) => {
                           setisLoading(true)
                           getBase64(res.target.files[0],res.target.value)    
-                      }}  className="opacity-0 w-full h-full absolute top-0" type="file" id="upload-button" multiple accept="*" />                        
+                      }}  className={`${theme}-FileUploader-uploader-input`} type="file" id="upload-button" multiple accept="*" />                        
                   </div>
             }
             {files.length > 0 ? 
               <>
                 <div>
-                  <div className="text-[#676767] text-sm mt-5">Uploaded files</div>
-                  <div className="overflow-auto mt-[6px] max-h-[150px]">
+                  <div className={`${theme}-FileUploader-itemList-titleBox`}>Uploaded files</div>
+                  <div className={`${theme}-FileUploader-itemList-items`}>
                     {files.map((item,index) => {
                       return (
                         <div className={`${theme}-FileUploader-uploadBox-file`}>
-                          <div className="text-sm text-[#0F0F0F]">{item.name}</div>
-                          <div onClick={() => deleteFile(index)} className="w-4 h-4 flex justify-center items-center cursor-pointer bg-[#1C1C1C] opacity-10 rounded-full">
+                          <div className={`${theme}-FileUploader-itemList-title`}>{item.name}</div>
+                          <div onClick={() => deleteFile(index)} className={`${theme}-FileUploader-uploadBox-trashIcon`}>
                             <img src="Acord/Trash.svg" alt="" />
                           </div>
                         </div>
@@ -75,7 +86,7 @@ const FileUploadr: React.FC<FileUploadrProps> = ({ children, theme, ...props }) 
               </>
             : undefined}
           </div>      
-          <div className="relative bottom-2 w-[-webkit-fill-available]">
+          <div className="Copilot-FileUploader-uploadBox-button-container">
             <Button disabled={files.length ==0} theme="Copilot">Continue</Button>
           </div>
     </div>
